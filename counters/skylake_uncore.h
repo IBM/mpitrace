6917bc2ecd722384d1686d5629676f4fc53b863e
Bob Walkup <walkup@us.ibm.com>
2020-01-27 14:05:24 -0500

    update skylake uncore counters

--- counters/skylake_uncore.h
+++ counters/skylake_uncore.h
@@ -1,5 +1,5 @@
 #define NUM_COUNTERS 6
-#define MAX_GROUPS 4
+#define MAX_GROUPS 6
 
 struct eventStruct {
                       char * name;
@@ -33,20 +33,38 @@ struct groupStruct CounterGroup[] =
   },  
 // group 2:
   {
-    "skx_unc_imc0::UNC_M_CAS_COUNT:WR:cpu=0", "writes from imc0 cpu=0",
-    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=0", "writes from imc1 cpu=0",
-    "skx_unc_imc2::UNC_M_CAS_COUNT:WR:cpu=0", "writes from imc2 cpu=0",
-    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=0", "writes from imc3 cpu=0",
-    "skx_unc_imc4::UNC_M_CAS_COUNT:WR:cpu=0", "writes from imc4 cpu=0",
-    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=0", "writes from imc5 cpu=0",
+    "skx_unc_imc0::UNC_M_CAS_COUNT:WR:cpu=0", "writes to imc0 cpu=0",
+    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=0", "writes to imc1 cpu=0",
+    "skx_unc_imc2::UNC_M_CAS_COUNT:WR:cpu=0", "writes to imc2 cpu=0",
+    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=0", "writes to imc3 cpu=0",
+    "skx_unc_imc4::UNC_M_CAS_COUNT:WR:cpu=0", "writes to imc4 cpu=0",
+    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=0", "writes to imc5 cpu=0",
   },  
 // group 3:
   {
-    "skx_unc_imc0::UNC_M_CAS_COUNT:WR:cpu=1", "writes from imc0 cpu=1",
-    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=1", "writes from imc1 cpu=1",
-    "skx_unc_imc2::UNC_M_CAS_COUNT:WR:cpu=1", "writes from imc2 cpu=1",
-    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=1", "writes from imc3 cpu=1",
-    "skx_unc_imc4::UNC_M_CAS_COUNT:WR:cpu=1", "writes from imc4 cpu=1",
-    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=1", "writes from imc5 cpu=1"
-  }   
+    "skx_unc_imc0::UNC_M_CAS_COUNT:WR:cpu=1", "writes to imc0 cpu=1",
+    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=1", "writes to imc1 cpu=1",
+    "skx_unc_imc2::UNC_M_CAS_COUNT:WR:cpu=1", "writes to imc2 cpu=1",
+    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=1", "writes to imc3 cpu=1",
+    "skx_unc_imc4::UNC_M_CAS_COUNT:WR:cpu=1", "writes to imc4 cpu=1",
+    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=1", "writes to imc5 cpu=1"
+  },
+// group 4:
+  {
+    "skx_unc_imc0::UNC_M_CAS_COUNT:RD:cpu=0", "reads from imc0 cpu=0",
+    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=0", "writes  to imc1 cpu=0",
+    "skx_unc_imc2::UNC_M_CAS_COUNT:RD:cpu=0", "reads from imc2 cpu=0",
+    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=0", "writes  to imc3 cpu=0",
+    "skx_unc_imc4::UNC_M_CAS_COUNT:RD:cpu=0", "reads from imc4 cpu=0",
+    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=0", "writes  to imc5 cpu=0"
+  },
+// group 5:
+  {
+    "skx_unc_imc0::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc0 cpu=0",
+    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc1 cpu=0",
+    "skx_unc_imc2::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc2 cpu=0",
+    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc3 cpu=0",
+    "skx_unc_imc4::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc4 cpu=0",
+    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc5 cpu=0"
+  }
 };
