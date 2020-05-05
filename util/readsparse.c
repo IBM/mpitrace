956f595b0b9487e2fddf163c7ec442bac7efc8f4
Bob Walkup <walkup@us.ibm.com>
2020-05-05 10:38:57 -0400

    updated the readsparse utility

--- util/readsparse.c
+++ util/readsparse.c
@@ -17,7 +17,8 @@ int main(int argc, char * argv[])
   FILE * fd;
   int nranks, i, k, nconn, minconn, maxconn;
   int minconn_rank, maxconn_rank, minbytes_rank, maxbytes_rank;
-  float totbytes, minbytes, maxbytes, avgbytes, avgconn;
+  int msgs, minmsgs, maxmsgs, minmsgs_rank, maxmsgs_rank;
+  float totbytes, minbytes, maxbytes, avgbytes, avgconn, avgmsgs;
 
 
   fd = fopen(argv[1], "r");
@@ -35,6 +36,9 @@ int main(int argc, char * argv[])
 
   maxbytes = 0.0;
   minbytes = 1.0e20;
+  minmsgs = 2000000000;
+  maxmsgs = 0;
+  avgmsgs = 0.0f;
 
   maxconn = 0;
   minconn = 10000000;
@@ -48,18 +52,30 @@ int main(int argc, char * argv[])
   avgconn = 0.0;
   avgbytes = 0.0;
 
+
   for (i=0; i<nranks; i++) {
      fread(&nconn, sizeof(int), 1, fd);
      fread(connection, sizeof(struct connStruct), nconn, fd);
      avgconn += (float) nconn;
      totbytes = 0.0;
+     msgs = 0;
      printf("rank %d has %d connections\n", i, nconn);
      for (k=0; k<nconn; k++) {
        totbytes += connection[k].bytes;
+       msgs += connection[k].count;
        printf("rank %d sent to rank %d : messages = %d, bytes = %.0f\n", i, connection[k].rank, connection[k].count, connection[k].bytes);
      }
      avgbytes += totbytes;
-     printf("rank %d sent a total of %.4e bytes\n", i, totbytes);
+     avgmsgs  += (float) msgs;
+     printf("rank %d sent a total of %.4e bytes using %d messages\n", i, totbytes, msgs);
+     if (msgs > maxmsgs) {
+       maxmsgs = msgs;
+       maxmsgs_rank = i;
+     }
+     if (msgs < minmsgs) {
+       minmsgs = msgs;
+       minmsgs_rank = i;
+     }
      if (totbytes > maxbytes) {
        maxbytes = totbytes;
        maxbytes_rank = i;
@@ -81,6 +97,7 @@ int main(int argc, char * argv[])
 
   avgconn  = avgconn  / ((float) nranks);
   avgbytes = avgbytes / ((float) nranks);
+  avgmsgs  = avgmsgs  / ((float) nranks);
 
   printf("average connections = %.3f\n", avgconn);
 
@@ -91,6 +108,11 @@ int main(int argc, char * argv[])
 
   printf("min bytes sent = %.4e for rank %d\n", minbytes, minbytes_rank);
   printf("max bytes sent = %.4e for rank %d\n", maxbytes, maxbytes_rank);
+
+  printf("min messages sent = %d for rank %d\n", minmsgs, minmsgs_rank);
+  printf("max messages sent = %d for rank %d\n", maxmsgs, maxmsgs_rank);
+
+  printf("avg messages sent = %.4e\n", avgmsgs);
 
   fclose(fd);
 
