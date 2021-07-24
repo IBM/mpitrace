72039bec85dc94b5be410d22d456608827fd2d97
Bob Walkup <walkup@us.ibm.com>
2021-07-24 17:53:23 -0400

    corrected skylake uncore counter label

--- counters/skylake_uncore.h
+++ counters/skylake_uncore.h
@@ -60,11 +60,11 @@ struct groupStruct CounterGroup[] =
   },
 // group 5:
   {
-    "skx_unc_imc0::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc0 cpu=0",
-    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc1 cpu=0",
-    "skx_unc_imc2::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc2 cpu=0",
-    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc3 cpu=0",
-    "skx_unc_imc4::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc4 cpu=0",
-    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc5 cpu=0"
+    "skx_unc_imc0::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc0 cpu=1",
+    "skx_unc_imc1::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc1 cpu=1",
+    "skx_unc_imc2::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc2 cpu=1",
+    "skx_unc_imc3::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc3 cpu=1",
+    "skx_unc_imc4::UNC_M_CAS_COUNT:RD:cpu=1", "reads from imc4 cpu=1",
+    "skx_unc_imc5::UNC_M_CAS_COUNT:WR:cpu=1", "writes  to imc5 cpu=1"
   }
 };
